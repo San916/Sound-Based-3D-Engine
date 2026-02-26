a4f7a3563c0527081474cfb276f366cdbe26b766
San Kang <sankang0916@gmail.com>
2026-02-26 01:01:02 -0800

    [ENG-1] Adding glfw, glm changed README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Echolocation-Game-Engine
 3D engine where the user, instead of using light to see, uses sonar.
+
+# Setup
+* Install Vulkan (1.4.328.0), glfw3 (3.4.1), glm (1.02) with vcpkg
+```
+vcpkg install vulkan glfw3 glm
+```
+* Create a `/build` directory in the project folder and `cd` into it
+* Run:
+```
+cmake ..
+cmake --build .
+```
+* To run engine
+```
+debug\engine
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    printf("HIII\n");
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    if (window == NULL) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
